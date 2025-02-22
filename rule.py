2664ce6d15ded9204e2577b30e70997ad6f49b65
bruno.sciolla <bruno.sciolla1@gmail.com>
2025-02-22 12:04:22 +0100

    type safe rule.py

--- rule.py
+++ rule.py
@@ -1,8 +1,10 @@
+from typing import List, Optional, Tuple
+
 # Returns : rest of the genome, the sequence read
-def readsegment(gen):
+def readsegment(gen: List[int]) -> Tuple[List[int], List[int]]:
     found = 0
     if len(gen) == 0:
-        return([], [])
+        return ([], [])
     for i in range(0, len(gen), 2):
         if gen[i] != 0:
             found = 1
@@ -10,15 +12,14 @@ def readsegment(gen):
     # i is the first 1 encountered OR at the end of the array gen
     if found == 0:
         i = i+2
-    return(gen[i+1:], gen[1:i:2])
+    return (gen[i+1:], gen[1:i:2])
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-
-def readrule(gen):
+def readrule(gen: List[int]) -> Tuple[Optional[List[int]], Optional[List[int]], Optional[List[int]], Optional[List[int]], List[int]]:
     genstore = gen
     if gen[0:2] != [1, 1]:
-        return(None, None, None, None, gen[1:])
+        return (None, None, None, None, gen[1:])
     gen = gen[2:]
     gen, predecessor = readsegment(gen)
     gen, successor = readsegment(gen)
@@ -32,38 +33,36 @@ def readrule(gen):
     else:
         codes = gen[:4]
         rest = gen[4:]
-    return(predecessor, successor, extra, codes, rest)
+    return (predecessor, successor, extra, codes, rest)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-
 class Rule:
-
-    def __init__(self):
-        self.predecessor = None
-        self.successor = None
-        self.extra = None
-        self.codes = None
+    def __init__(self) -> None:
+        self.predecessor: Optional[List[int]] = None
+        self.successor: Optional[List[int]] = None
+        self.extra: Optional[List[int]] = None
+        self.codes: Optional[List[int]] = None
 
     # ------------------------------------------------ #
 
-    def build_from_adn(self, adn):
+    def build_from_adn(self, adn: List[int]) -> List[int]:
         predecessor, successor, extra, codes, rest = readrule(adn)
         if predecessor is not None:
             self.predecessor = predecessor
             self.successor = successor
             self.extra = extra
             self.codes = codes
-        return(rest)
+        return rest
 
     # ------------------------------------------------ #
 
-    def isRule(self):
-        return(self.predecessor is not None)
+    def isRule(self) -> bool:
+        return self.predecessor is not None
 
     # ------------------------------------------------ #
 
-    def print(self):
+    def print(self) -> None:
         print("Pred: ", self.predecessor)
         print("Succ: ", self.successor)
         print("Extra: ", self.extra)
